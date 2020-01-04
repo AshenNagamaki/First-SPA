53e07d626bd4e68560bbd4bb5db0524c9ee3755a
AshenNagamaki <ashennagamaki@protonmail.com>
2020-01-04 16:18:31 +0200

    Add basic implementation of Cart

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -1,13 +1,65 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 import classes from './Cart.module.css';
 import { useStore } from '../../hooks-store/store';
 
 const Cart = () => {
     const [state, dispatch] = useStore();
-    console.log(state)
+    const cartItemsQuantity = {};
+    for (let cartItem of [...state.cartItems]) {
+        if (cartItemsQuantity.hasOwnProperty(cartItem.title)) {
+            cartItemsQuantity[cartItem.title] ++;
+        } else {
+            cartItemsQuantity[cartItem.title] = 1;
+        };
+    };
+    if (Object.keys(cartItemsQuantity).length > 5) {
+    };
+    const cartItemsToShow = [...state.cartItems].filter((cartItem, index, self) => 
+        index === self.findIndex(cI => cI.title === cartItem.title)
+    );
+    const removeItemFromCartHandler = product => {
+        dispatch('REMOVE_SINGLE_FROM_CART', product);
+    };
+    const removeAllItemsFromCartHandler = product => {
+        dispatch('REMOVE_ALL_FROM_CART', product);
+    };
+    let cartInsides;
+    if (state.cartItems.length) {
+        cartInsides = cartItemsToShow.map(
+            (cartInside, index) => {
+                return (
+                    <tr className={classes.CartInside} key={cartInside.imageAlt + index}>
+                        <td><img src={cartInside.imageSource} alt={cartInside.imageAlt}/></td>
+                        <td><p><strong>{cartInside.title.toUpperCase()} ({cartItemsQuantity[cartInside.title]})</strong></p><p>{cartInside.country} ({cartInside.madeBy})</p></td>
+                        <td>$<strong>{cartInside.price}</strong></td>
+                        <td><button onClick={() => removeItemFromCartHandler(cartInside)}>REMOVE</button></td>
+                        <td><button onClick={() => removeAllItemsFromCartHandler(cartInside)}>REMOVE ALL</button></td>
+                    </tr>
+                );
+            }
+        );
+    };
+
     return (
-        <div className={classes.Cart}></div>
+        <div className={classes.Cart}>
+            <table>
+                <thead>
+                    <tr>
+                        <th className={classes.CartTableHeaders}>Description</th>
+                        <th className={classes.CartTableHeaders}>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cartInsides}
+                </tbody>
+            </table>
+            <h1 className={classes.EmptyCart}>YOUR CART IS CURRENTLY EMPTY</h1>
+            <h1><NavLink className={classes.BackToShop} to="/products">BACK TO SHOP</NavLink></h1>
+            <h1>TOTAL <span className={classes.Currency}>USD</span> ${state.totalPrice}</h1>
+            <h1>CONTINUE TO PAYMENT OR YOU CAN BACK TO <NavLink className={classes.BackToShop} to="/products">SHOP</NavLink></h1>
+        </div>
     );
 };
 
--- src/hooks-store/cart-store.js
+++ src/hooks-store/cart-store.js
@@ -3,19 +3,39 @@ import { initStore } from './store';
 const configureStore = () => {
     const actions = {
         ADD_TO_CART: (curState, product) => {
-            curState.cartItems.push(product);
-            return { cartItems: curState.cartItems }
+            const updatedCartItems = [...curState.cartItems];
+            updatedCartItems.push(product);
+            const updatedTotalPrice = curState.totalPrice + product.price;
+            return { cartItems: updatedCartItems, totalPrice: updatedTotalPrice }
         },
-        REMOVE_FROM_CART: (curState, product) => {
-            
-            return { cartItems: curState.updatedCartItems }
+        REMOVE_SINGLE_FROM_CART: (curState, product) => {
+            const updatedCartItems = [...curState.cartItems];
+            let updatedTotalPrice;
+            for (let cartItem of updatedCartItems) {
+                if (cartItem.title === product.title) {
+                    updatedCartItems.splice(curState.cartItems.indexOf(cartItem), 1);
+                    updatedTotalPrice = curState.totalPrice - product.price;
+                    break;
+                };
+            };
+            return { cartItems: updatedCartItems, totalPrice: updatedTotalPrice}
+        },
+        REMOVE_ALL_FROM_CART: (curState, product) => {
+            let updatedTotalPrice = curState.totalPrice;
+            for (let cartItem of [...curState.cartItems]) {
+                if (cartItem.title === product.title) {
+                    updatedTotalPrice -= product.price;
+                };
+            };
+            const updatedCartItems = [...curState.cartItems].filter(cartItem => cartItem.title !== product.title);
+            return { cartItems: updatedCartItems, totalPrice: updatedTotalPrice}
         },
         CLEAR_CART: curState => {
-            curState.cartItems = [];
-            return { cartItems: curState.updatedCartItems}
+            curState.cartItems.splice(0, curState.cartItems.length);
+            return { cartItems: curState.cartItems, totalPrice: 0 }
         }
     };
-    initStore(actions, {cartItems: []});
+    initStore(actions, {cartItems: [], totalPrice: 0});
 };
 
-export default configureStore; 
+export default configureStore;
